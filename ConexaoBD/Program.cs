f7fece17ed0ce8f613c548f4922f78610b117057
Asael <asael.guitar_g3@hotmail.com>
2019-11-27 20:13:26 -0300

    Comando SELECT

--- ConexaoBD/Program.cs
+++ ConexaoBD/Program.cs
@@ -11,7 +11,22 @@ namespace ConexaoBD
     {
         static void Main(string[] args)
         {
-            SqlConnection
+            SqlConnection conexao = new SqlConnection(@"Data Source= DESKTOP-O34D68D\SQLEXPRESS; Integrated Security=SSPI; Initial Catalog=ExemploBD");
+            conexao.Open();
+
+            string strQuerySelect = "SELECT * FROM Funcionario";
+            SqlCommand comandoSelect = new SqlCommand(strQuerySelect, conexao);
+            SqlDataReader executaComando = comandoSelect.ExecuteReader();
+            //SqlDataReader: Executor de dados, usado no comando SELECT
+
+
+            //Read() : em quanto ouver dados faça a leitura
+            while (executaComando.Read())
+            {
+                Console.WriteLine("Id: {0}, Nome: {1},Cargo: {2}, Data-Nasc: {3}, E-mail {4},", executaComando["usuarioId"], executaComando["nome"], executaComando["cargo"], executaComando["date"], executaComando["email"]);
+            }
+
+            Console.ReadLine();
         }
     }
 }
