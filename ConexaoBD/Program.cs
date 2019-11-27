95c38ff6bbed91a57b9de64c3181173d54056b75
Asael <asael.guitar_g3@hotmail.com>
2019-11-27 20:26:22 -0300

    Comando UPDATE

--- ConexaoBD/Program.cs
+++ ConexaoBD/Program.cs
@@ -14,11 +14,17 @@ namespace ConexaoBD
             SqlConnection conexao = new SqlConnection(@"Data Source= DESKTOP-O34D68D\SQLEXPRESS; Integrated Security=SSPI; Initial Catalog=ExemploBD");
             conexao.Open();
 
+            string strQueryUpdate = "UPDATE Funcionario SET nome = 'Luan' WHERE usuarioId = 5";
+            SqlCommand comandoUpdate = new SqlCommand(strQueryUpdate, conexao);
+            comandoUpdate.ExecuteNonQuery();
+            //ExecuteNonQuery(): não tem retorno
+
+
             string strQuerySelect = "SELECT * FROM Funcionario";
             SqlCommand comandoSelect = new SqlCommand(strQuerySelect, conexao);
             SqlDataReader executaComando = comandoSelect.ExecuteReader();
             //SqlDataReader: Executor de dados, usado no comando SELECT
-
+            //ExecuteReader(): retorna algo do banco
 
             //Read() : em quanto ouver dados faça a leitura
             while (executaComando.Read())
