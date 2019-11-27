d3d59b41016974d2f1e56534200772affa0a96b7
Asael <asael.guitar_g3@hotmail.com>
2019-11-27 20:48:12 -0300

    Comando DELETE e INSERT

--- ConexaoBD/Program.cs
+++ ConexaoBD/Program.cs
@@ -14,10 +14,31 @@ namespace ConexaoBD
             SqlConnection conexao = new SqlConnection(@"Data Source= DESKTOP-O34D68D\SQLEXPRESS; Integrated Security=SSPI; Initial Catalog=ExemploBD");
             conexao.Open();
 
-            string strQueryUpdate = "UPDATE Funcionario SET nome = 'Luan' WHERE usuarioId = 5";
-            SqlCommand comandoUpdate = new SqlCommand(strQueryUpdate, conexao);
-            comandoUpdate.ExecuteNonQuery();
-            //ExecuteNonQuery(): não tem retorno
+            //string strQueryUpdate = "UPDATE Funcionario SET nome = 'Luan' WHERE usuarioId = 5";
+            //SqlCommand comandoUpdate = new SqlCommand(strQueryUpdate, conexao);
+            //comandoUpdate.ExecuteNonQuery();
+            ////ExecuteNonQuery(): não tem retorno
+
+
+            //string strQueryDelete = "DELETE FROM Funcionario WHERE usuarioId = 5";
+            //SqlCommand comandoDelete = new SqlCommand(strQueryDelete, conexao);
+            //comandoDelete.ExecuteNonQuery();
+
+            Console.WriteLine("Digite o nome do Funcionario: ");
+            string nome = Console.ReadLine(); // Ira guardar o que o usuario digitou no console dento da variavel
+            
+            Console.WriteLine("Digite Cargo: ");
+            string cargo = Console.ReadLine();// Ira guardar o que o usuario digitou no console dento da variavel
+
+            Console.WriteLine("Digite Data nascimento: ");
+            string dataNasc = Console.ReadLine();// Ira guardar o que o usuario digitou no console dento da variavel
+
+            Console.WriteLine("Digite o email: ");
+            string email = Console.ReadLine();// Ira guardar o que o usuario digitou no console dento da variavel
+
+            string strQueryInsert = string.Format("INSERT INTO Funcionario(nome, cargo, date, email) VALUES('{0}','{1}', '{2}', '{3}')", nome, cargo, dataNasc, email);
+            SqlCommand comandoInsert = new SqlCommand(strQueryInsert, conexao);
+            comandoInsert.ExecuteNonQuery();
 
 
             string strQuerySelect = "SELECT * FROM Funcionario";
