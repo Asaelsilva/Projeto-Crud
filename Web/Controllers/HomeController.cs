1ca9f83c3a95bb1e1b968af2e7bd76f2b91d07e5
Asael <asael.guitar_g3@hotmail.com>
2019-11-29 09:37:46 -0300

    Preparando controller e view para receber Lista de Funcionarios

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BDPojetoCrud.Aplicacao;
 
 namespace Web.Controllers
 {
@@ -10,21 +11,25 @@ namespace Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+
+            var appFuncionario = new FuncionarioAplicacao();
+            var listaFuncionario = appFuncionario.Lista();
+
+            return View(listaFuncionario);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+        //    return View();
+        //}
     }
 }
