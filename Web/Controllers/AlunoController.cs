6444401738e29ba55dd64689c45d3a5871cfe89b
Asael <asael.guitar_g3@hotmail.com>
2019-11-29 10:38:11 -0300

    Gerando Scaffolding de list

---
+++ Web/Controllers/AlunoController.cs
@@ -0,0 +1,20 @@
+﻿using BDPojetoCrud.Aplicacao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class AlunoController : Controller
+    {
+        // GET: Aluno
+        public ActionResult Index()
+        {
+            var appFuncionario = new FuncionarioAplicacao();
+            var listaFuncionario = appFuncionario.Lista();
+            return View(listaFuncionario);
+        }
+    }
+}
